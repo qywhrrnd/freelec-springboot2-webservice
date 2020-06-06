0e3f319263a394b063a3d293b1642a41b50785f2
qywhrrnd <qywhrrnd777@naver.com>
2020-06-06 20:26:28 +0900

    발라드 크롤링 성공!

---
+++ src/main/java/com/jojoldu/book/service/PostsService.java
@@ -0,0 +1,17 @@
+package com.jojoldu.book.service;
+
+import com.jojoldu.book.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
